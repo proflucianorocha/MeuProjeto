62be33477e6a2992ecac0603376df470ed7264c0
Luciano Rocha <lucianorocha@ifsul.edu.br>
2021-02-17 18:41:12 -0200

    Código fonte

--- src/meuprojetocompartilhado/MeuProjetoCompartilhado.java
+++ src/meuprojetocompartilhado/MeuProjetoCompartilhado.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package meuprojetocompartilhado;
 
-/**
- *
- * @author lucianorocha
- */
+
 public class MeuProjetoCompartilhado {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+        
+        System.out.println("Ola mundo");
+    
+}
 }
